a6561c93dd59e0e422c24a84f9a782e16311b00a
alutskevich <karantin91@gmail.com>
2017-03-30 03:19:46 +0300

    change service provider to the facade

---
+++ src/TableContentsFacade.php
@@ -0,0 +1,17 @@
+<?php
+namespace Lutskevich\TOC;
+
+use Illuminate\Support\Facades\Facade;
+
+class TableContentsFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return TableContents::class;
+    }
+}
